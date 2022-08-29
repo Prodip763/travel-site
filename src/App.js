51249f1b724b2176fe1e19c2913a1349f0755f56
prodip763 <prodip12763@gmail.com>
2022-08-29 06:28:03 +0600

    login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Services from './Pages/Home/Services/Services';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/services' element={<Services></Services>}></Route>
         <Route path='/service/:detail' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
+import {Form, Button} from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
 
-const Login = () =>{
-    return(
-        <div>
+const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate()
+
+    const handleSubmit =e =>{
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
+
+    const navigateRegister = () =>{
+        navigate('/register');
+    }
+
+    return (
+        <div className="container w-50 mx-auto">
             <h1>please Login!!</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter Your email" required />
+                    
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+                    <Form.Text className="text-muted">
+                        We'll never share your password with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Button className="mx-auto d-block" variant="danger" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <p>New to Travel Site? <span className="text-danger "style={{cursor:'pointer'}} onClick={navigateRegister}>Please Signup</span></p>
         </div>
     );
 };
