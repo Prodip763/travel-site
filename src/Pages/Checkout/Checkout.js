acaadabf1cb32ffeb63b3ebb471edbe55794f77f
prodip763 <prodip12763@gmail.com>
2022-08-31 03:47:28 +0600

    bookin

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Checkout.css";
+// import { Selector } from "./selector";
 
 const Checkout = () =>{
     return(
-        <div>
-            <h2>please checkout your booking</h2>
-        </div>
+        <div className="App px-4" style={{ height: "2000px" }}>
+      <h1 className="mt-8 text-gray-500 uppercase bold text-2xl">
+        Please checkout your booking
+      </h1>
+      <h2 className="mb-12">Start editing to see some magic happen!</h2>
+      {/* <Selector /> */}
+    </div>
     );
 };
 
